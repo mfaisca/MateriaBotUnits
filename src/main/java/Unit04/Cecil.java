288c7d09a68fb88bcca5f5f7b99b7e6265b0b7de
Quetz <Quetz@REACTOR.home>
2022-04-07 01:36:19 +0100

    Name Fixes

---
+++ src/main/java/Unit04/Cecil.java
@@ -0,0 +1,8 @@
+package Unit04;
+import com.materiabot.GameElements.*;
+import org.plugface.core.annotations.Plugin;
+
+@Plugin(name = "Unit.Cecil")
+public class Cecil extends Unit{
+	public Cecil() { super("Cecil"); }
+}
---
+++ src/main/java/UnitOther/LannReynn.java
@@ -0,0 +1,8 @@
+package UnitOther;
+import com.materiabot.GameElements.*;
+import org.plugface.core.annotations.Plugin;
+
+@Plugin(name = "Unit.Lann&Reynn")
+public class LannReynn extends Unit{
+	public LannReynn() { super("Lann & Reynn", "Lann&Reynn"); }
+}
