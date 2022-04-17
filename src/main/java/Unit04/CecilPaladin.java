65700cf1d980e389ec8bf291adb4ad80f1616abc
Quetz <Quetz@REACTOR.home>
2022-04-18 00:19:57 +0100

    .

---
+++ src/main/java/Unit04/CecilPaladin.java
@@ -0,0 +1,8 @@
+package Unit04;
+import com.materiabot.GameElements.*;
+import org.plugface.core.annotations.Plugin;
+
+@Plugin(name = "Unit.Cecil_Paladin")
+public class CecilPaladin extends Unit{
+	public CecilPaladin() { super("Cecil Paladin"); }
+}
